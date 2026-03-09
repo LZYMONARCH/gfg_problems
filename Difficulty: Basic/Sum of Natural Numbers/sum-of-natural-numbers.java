50be25b75a07ba6578eafa3585e356bd63f506d6
SUJAL GUPTA <sujalgupta841@gmail.com>
2026-03-09 09:24:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int findSum(int n) {
+        // code here
+        if(n==0){
+            return 1;
+            
+        } 
+        else{
+            return (n*(n+1))/2 ;     
+            }
+        }
+}
