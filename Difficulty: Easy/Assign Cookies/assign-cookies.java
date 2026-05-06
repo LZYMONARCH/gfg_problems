197e30c3479e5111f0fd1638e2d24a671cd8af3d
SUJAL GUPTA <sujalgupta841@gmail.com>
2026-05-06 09:33:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Assign Cookies/assign-cookies.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxChildren(int[] greed, int[] cookie) {
+
+        Arrays.sort(greed);
+        Arrays.sort(cookie);
+
+        int i = 0;
+        int j = 0; 
+
+        while (i < greed.length && j < cookie.length) {
+            if (cookie[j] >= greed[i]) {
+                i++;
+            }
+            j++;
+        }
+
+        return i;
+    }
+}
